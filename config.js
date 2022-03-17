be685fdb4fa8aba8794d4340d17d8b46dae0f8e3
Mr.C1TY3920 <44989007+CAMRYBOY45@users.noreply.github.com>
2022-03-17 17:16:46 +0700

    Update config.js

--- config.js
+++ config.js
@@ -18,8 +18,8 @@ global.APIKeys = {
 }
 
 // Other
-global.owner = ['6283167714830','6281382420312','6288292024190']
-global.premium = ['6288292024190']
+global.owner = ['62881024042010']
+global.premium = ['62881024042010']
 global.packname = 'Hisoka Morrow'
 global.author = 'WhatsApp Bot'
 global.sessionName = 'hisoka'
